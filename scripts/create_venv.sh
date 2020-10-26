f800640740a53fddcbe91295ae16d85729d1e4df
Carlos Eguiluz Rosas <ceguiluzrosas@gmail.com>
2020-10-26 16:47:23 -0400

    removing moved-files and simplifying a few lines

--- scripts/create_venv.sh
+++ scripts/create_venv.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # Assuming python virtual env is already installed
-# Remember to : chmod 755 create_venv.sh
 
 LOCATION=$1
 ALIAS=$2
 
 if python3 -m venv $LOCATION/venv ; then
-    echo "alias $ALIAS='cd $LOCATION && source venv/bin/activate'" >> ~/.bash_profile
+    echo "alias $ALIAS='cd $LOCATION && source venv/bin/activate'" >> ~/.bashrc
     echo "Congrats :) venv has been created!"
 else
     echo "Oh no :( There was an error ..."
--- setup.sh
+++ setup.sh
@@ -10,15 +10,12 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Installing Packages with Brew
-brew install git # git
-brew install hub # github package
-brew install the_silver_searcher # faster way to find keywords
-brew install python3 # latest version of python3
+while read package; do brew install "$package" ; done < ./packages/homebrew_packages.txt
 
 ##########################
 ### Create Directories ###
 ##########################
-SCRIPTS_DIR="$HOME/ScriptsDir"; echo $SCHOOL_DIR
+SCRIPTS_DIR="$HOME/ScriptsDir"; echo $SCHOOL_DIR; chmod -R 755 $SCRIPTS_DIR
 WORK_DIR="$HOME/WorkDir" ; echo $WORK_DIR
 SCHOOL_DIR="$HOME/SchoolDir"; echo $SCHOOL_DIR
 SCREEN_SHOTS_DIR="$HOME/Desktop/Screen_Shots"; echo $SCREEN_SHOTS_DIR
@@ -34,22 +31,26 @@ defaults write com.apple.screencapture location $SCREEN_SHOTS_DIR
 ### Developer Things ###
 ########################
 pip3 install --upgrade pip --user
-python3 -m pip install --user -r pip_requirements.txt
+python3 -m pip install --user -r ./packages/pip_requirements.txt
 
-# autocomplete
+# HTTPS to Git/SSH protocols
+git config --global hub.protocol https
+
+# Autocomplete
 git clone git://github.com/ajh17/VimCompletesMe.git ~/.vim/pack/vendor/start/VimCompletesMe
 
-# sea shell theme for terminal
+# Sea Shell Theme for terminal
 curl -o ~/Desktop/SeaShells.terminal 'https://raw.githubusercontent.com/lysyi3m/macos-terminal-themes/master/schemes/SeaShells.terminal'
 
-# vim completes me
+# Vim Completes Me
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
-# adding profiles
-cat ./bash_profile.txt >> ~/.bashrc
-cat ./vim_profile.txt >> ~/.vimrc
-cat ./zsh_profile.txt >> ~/.zshrc
+# Adding Profiles
+cat ./profiles/rfiles/bash_profile.txt >> ~/.bashrc
+cat ./profiles/vim_profile.txt >> ~/.vimrc
+cat ./profiles/zsh_profile.txt >> ~/.zshrc
 
-# installing atom packages
-while read package; do apm install "$package" ; done < atom_packages.txt
+# Installing Editor Packages
+while read package; do apm install "$package" ; done < ./packages/atom_packages.txt
+while read package; do code --install-extension "$package" ; done < ./packages/vscode_packages.txt
